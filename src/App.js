3deccd25f590c952a0c0d6942eec8f8e7fed4fbe
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-16 16:59:17 -0600

    added button for - and + with span

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
+import { useState } from 'react'
+
 function App() {
+
+  const [] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
     </div>
   );
 }
